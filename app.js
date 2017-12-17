393f2ae2cb080964cde537090d4bb44c95a5ebe5
System Administrator <root@Neelams-MacBook-Pro.local>
2017-12-17 22:50:09 +0530

    handled google auth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
-
-const port = process.env.PORT || 9000; //process.env.PORT for production port
 
 // '/' route
 app.get('/',(req,res)=>{
   res.send('It Works!');
 });
+
+//Use routes
+app.use('/auth',auth);
+
+const port = process.env.PORT || 9000; //process.env.PORT for production port
+
 
 app.listen(port,()=>console.log(`Server set at ${port}`));
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  monggoURI: 'mongodb://abhi_mLab:swsdata@ds139446.mlab.com:39446/storiesworthsharing-dev',
+  googleClientID: '723961887347-66ullb71uoi1qjp5o6v7unuu0jginelt.apps.googleusercontent.com',
+  googleClientSecret: 'rwkxP74RRt_PwxfudReRhgF2'
+}
